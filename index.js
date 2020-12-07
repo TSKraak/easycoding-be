d6ae0ecc8fbfee450dfd8b05de5fe29cb6d46ca1
ariqyandri <ariq140199@gmail.com>
2020-12-07 17:07:08 +0100

    Debug auth router

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = express.json();
 const cors = require("cors");
 const { PORT } = require("./config/constants");
+const authRouter = require("./routers/auth");
 const postRouter = require("./routers/post");
 const requestRouter = require("./routers/request");
 const commentRouter = require("./routers/comment");
@@ -13,6 +14,8 @@ app.use(bodyParser);
 app.get("/", (req, res) => {
   res.send("Hi from Express, this is the backend of Easy Coding!!!");
 });
+
+app.use("/", authRouter);
 
 app.use("/post", postRouter);
 

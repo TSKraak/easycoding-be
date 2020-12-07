db222ab0aeac5fc1f1f71819c930fa9f746e3568
JANNIS-PASSALIS <PASALIS13@HOTMAIL.COM>
2020-12-07 12:46:39 +0100

    Setup express

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hi from express, this is the backend of Easy Coding!!!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});

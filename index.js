0bd2dbb4e003c0346ee8d6cc97cdd209b771039e
ariqyandri <ariq140199@gmail.com>
2020-12-07 16:25:13 +0100

    Add routers to index

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = express.json();
 const cors = require("cors");
 const { PORT } = require("./config/constants");
+const postRouter = require("./routers/post");
+const requestRouter = require("./routers/request");
 const app = express();
 
 app.use(cors());
@@ -10,6 +12,10 @@ app.use(bodyParser);
 app.get("/", (req, res) => {
   res.send("Hi from Express, this is the backend of Easy Coding!!!");
 });
+
+app.use("/post", postRouter);
+
+app.use("/request", requestRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);

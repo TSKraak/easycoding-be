eaa405338bedd7b3bf5e45333721b7ca9eff8b3c
JANNIS-PASSALIS <PASALIS13@HOTMAIL.COM>
2020-12-07 13:34:36 +0100

    Add cors and body parser

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const bodyParser = express.json();
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(bodyParser);
 
 app.get("/", (req, res) => {
   res.send("Hi from express, this is the backend of Easy Coding!!!");

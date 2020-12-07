37a54f239901fdfb7806c29837f32c786f62f72a
JANNIS-PASSALIS <PASALIS13@HOTMAIL.COM>
2020-12-07 12:41:57 +0100

    Create request model/table

---
+++ models/request.js
@@ -0,0 +1,25 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class request extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  request.init(
+    {
+      title: { type: DataTypes.STRING, allowNull: false },
+      content: { type: DataTypes.STRING, allowNull: false },
+    },
+    {
+      sequelize,
+      modelName: "request",
+    }
+  );
+  return request;
+};

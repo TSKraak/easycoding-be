3652f325b7d8cdacf9eeba1ab470fb40ab756e15
ariqyandri <ariq140199@gmail.com>
2020-12-09 10:57:13 +0100

    Create user update router

--- routers/user.js
+++ routers/user.js
@@ -12,11 +12,32 @@ router.post("/", authMiddleware, async (req, res, next) => {
         .status(403)
         .send({ message: "You are not authorized to see all users" });
     }
-    const users = await User.findAll();
+    const users = await User.findAll({ attributes: { exclude: ["password"] } });
     res.status(200).json({
       users,
       message: "Admin Found. You are authorized to see all users",
     });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.put("/", authMiddleware, async (req, res, next) => {
+  const { name, email, picture } = req.body;
+  try {
+    const updatedUser = await User.update(
+      {
+        name,
+        email,
+        picture,
+      },
+      { where: { id: req.user.dataValues["id"], return: true } }
+    );
+    const returnUser = await User.findOne({
+      where: updatedUser,
+      attributes: { exclude: ["password"] },
+    });
+    res.status(200).json({ returnUser });
   } catch (e) {
     next(e);
   }
@@ -28,7 +49,7 @@ router.put("/block/:id", authMiddleware, async (req, res, next) => {
     if (admin === false) {
       return res
         .status(403)
-        .send({ message: "You are not authorized to see all users" });
+        .send({ message: "You are not authorized to block users" });
     }
     const user = await User.findByPk(parseInt(req.params.id));
     await user.update({

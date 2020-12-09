562379c9e9b77c8c1ea9833b2139b65ca9e650c0
ariqyandri <ariq140199@gmail.com>
2020-12-09 12:26:56 +0100

    Update favourite router

--- routers/favourite.js
+++ routers/favourite.js
@@ -11,9 +11,57 @@ const {
 
 const router = new Router();
 
-router.get("/", async (req, res, next) => {
+router.get("/", authMiddleware, async (req, res, next) => {
   try {
-    const response = await Post.findAll({
+    const response = await User.findByPk(req.user.dataValues["id"], {
+      attributes: ["id"],
+      include: [
+        {
+          model: Post,
+          as: "favourite",
+          include: [
+            {
+              model: Comment,
+              include: [
+                {
+                  model: Answer,
+                  include: [
+                    {
+                      model: User,
+                      attributes: { exclude: ["password"] },
+                    },
+                  ],
+                },
+                {
+                  model: User,
+                  attributes: { exclude: ["password"] },
+                },
+              ],
+            },
+            {
+              model: User,
+              as: "author",
+              attributes: { exclude: ["password"] },
+            },
+            { model: Picture },
+          ],
+        },
+      ],
+    });
+    res.status(200).json(response);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", authMiddleware, async (req, res, next) => {
+  const { postId } = req.body;
+  try {
+    const newFavourite = await Favourite.create({
+      postId,
+      userId: req.user.dataValues["id"],
+    });
+    const returnFavouritedPost = await Post.findByPk(postId, {
       include: [
         {
           model: Comment,
@@ -33,43 +81,15 @@ router.get("/", async (req, res, next) => {
             },
           ],
         },
-        { model: User, as: "author", attributes: { exclude: ["password"] } },
+        {
+          model: User,
+          as: "author",
+          attributes: { exclude: ["password"] },
+        },
         { model: Picture },
       ],
     });
-    res.status(200).json(response);
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.post("/", authMiddleware, async (req, res, next) => {
-  const { title, content } = req.body;
-  if (!title || !content) {
-    return res
-      .status(400)
-      .send({ message: "Please provide a title and content" });
-  }
-  try {
-    const newPost = await Post.create({
-      title,
-      content,
-      userId: req.user.dataValues["id"],
-    });
-    const assignedPictures = await Picture.update(
-      {
-        postId: newPost.id,
-      },
-      { where: { id: [...req.body.picturesIds] } }
-    );
-    const returnPost = await Post.findByPk(newPost.id, {
-      include: [
-        { model: Comment, include: [{ model: Answer }] },
-        { model: User, as: "author", attributes: { exclude: ["password"] } },
-        { model: Picture },
-      ],
-    });
-    res.status(200).send(returnPost);
+    res.status(200).send(returnFavouritedPost);
   } catch (error) {
     next(error);
   }

6762bec6e20d7734fcc970064e2b964d7849f1b6
ariqyandri <ariq140199@gmail.com>
2020-12-10 11:55:43 +0100

    Create PUT and DELETE answer router

--- routers/answer.js
+++ routers/answer.js
@@ -27,4 +27,65 @@ router.post("/", authMiddleware, async (req, res, next) => {
   }
 });
 
+router.put("/:answerId", authMiddleware, async (req, res, next) => {
+  const { content } = req.body;
+  if (!content) {
+    return res.status(400).send({ message: "Please provide a content" });
+  }
+  try {
+    const updatedAnswer = await Answer.update(
+      {
+        content,
+      },
+      { where: { id: parseInt(req.params.answerId) } }
+    );
+    const returnAnswer = await Answer.findByPk(parseInt(req.params.answerId), {
+      include: [
+        { model: User, attributes: { exclude: ["password"] } },
+        { model: Answer },
+      ],
+    });
+    res.status(200).send(returnAnswer);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:answerId", authMiddleware, async (req, res, next) => {
+  try {
+    await Answer.destroy({
+      where: {
+        id: parseInt(req.params.answerId),
+        userId: req.user.dataValues["id"],
+      },
+    });
+    res
+      .status(200)
+      .send({ message: `Deleted reply with id:${req.params.answerId}` });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/admin/:answerId", authMiddleware, async (req, res, next) => {
+  const admin = req.user.dataValues.isAdmin;
+  if (admin === false) {
+    return res
+      .status(403)
+      .send({ message: "You are not authorized to delete this reply" });
+  }
+  try {
+    await Answer.destroy({
+      where: {
+        id: parseInt(req.params.answerId),
+      },
+    });
+    res
+      .status(200)
+      .send({ message: `Deleted reply with id:${req.params.answerId}` });
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router;
